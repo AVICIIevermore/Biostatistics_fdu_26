728ad8aa9e86cfc4209846d075498aed9d57874c
LRJ <2576277959@qq.com>
2026-05-27 22:39:12 +0800

    Local updates on lrj-covariance_matrix

---
+++ lrj-covariance_matrix/Reproduction Time Comparison with New Method/run_extract.bat
@@ -0,0 +1,3 @@
+@echo off
+call D:\miniconda3\Scripts\activate.bat mmmd_boost
+Rscript --no-save --vanilla "D:\Fudan\GitHub repositories\MMMD-boost-kernel-two-sample\Reproduction Time Comparison with New Method\extract_numbers.R"
